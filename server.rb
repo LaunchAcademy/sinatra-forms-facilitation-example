ea6eaa24badf92694464111e949ad44fe278a875
nwalberts <nwalberts5@gmail.com>
2020-06-16 12:17:40 -0400

    updated codebase

--- server.rb
+++ server.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
+require "csv"
 
 configure :development, :test do
   require 'pry'
@@ -10,11 +12,26 @@ Dir[File.join(File.dirname(__FILE__), 'lib', '**', '*.rb')].each do |file|
   also_reload file
 end
 
-get '/greetings/new' do
+# ------
+
+get "/greetings" do
+  @greetings = CSV.readlines("greetings.csv")
+
   erb :index
 end
 
+get '/greetings/new' do
+  erb :new
+end
+
 post '/greetings' do
-  binding.pry
-  redirect '/greetings/new'
+  new_message = params[:message_body]
+  new_message_type = params[:message_type]
+
+
+  CSV.open("greetings.csv", "a") do |csv|
+    csv << [new_message, new_message_type]
+  end
+
+  redirect '/greetings'
 end
